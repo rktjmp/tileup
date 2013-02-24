f7d43fe3ac3cfb79d671b04fdc9d02299c8fffe4
omarriott <hello omarriott>
2013-02-24 17:00:39 +1100

    Added verbose flag, some dead code cleanup, error checking

--- lib/tileup.rb
+++ lib/tileup.rb
@@ -11,13 +11,27 @@ module TileUp
         tile_width: 256,
         tile_height: 256,
         filename_prefix: "map_tile",
-        output_dir: "."
+        output_dir: ".",
+        verbose: false
       }
       @options = OpenStruct.new(default_options.merge(options))
-
-      @image = Magick::Image::read(image_filename).first
       @extension = image_filename.split(".").last
       @filename_prefix = @options.filename_prefix
+
+      begin
+        @image = Magick::Image::read(image_filename).first        
+      rescue Magick::ImageMagickError => e
+        puts "Could not open image #{image_filename}."
+        exit
+      end
+
+      if @options.auto_zoom_levels && @options.auto_zoom_levels > 20
+        puts "Warning: auto zoom levels hard limited to 20."
+        @options.auto_zoom_levels = 20
+      end
+      if @options.auto_zoom_levels && @options.auto_zoom_levels <= 0
+        @options.auto_zoom_levels = nil
+      end
 
       puts "Opened #{image_filename}, #{@image.columns} x #{@image.rows}"
 
@@ -47,52 +61,33 @@ module TileUp
             output_dir: File.join(@options.output_dir, zoom_name.to_s),
             scale: scale
           }
-        end
-
-        # run through tasks list
-        tasks.each do |task|
-          image = @image
-          image_path = File.join(task[:output_dir], @filename_prefix)
-          if task[:scale] != 1.0 
-            # if scale required, scale image
-            image = @image.scale(task[:scale])
-          end
-          # make output dir
-          make_path(task[:output_dir])
-          self.make_tiles(image, image_path, @options.tile_width, @options.tile_height)
-        end
-                
+        end                
       end
 
-      puts tasks
+      # run through tasks list
+      tasks.each do |task|
+        image = @image
+        image_path = File.join(task[:output_dir], @filename_prefix)
+        if task[:scale] != 1.0 
+          # if scale required, scale image
+          begin
+            image = @image.scale(task[:scale])  
+          rescue RuntimeError => e
+            message = "Failed to scale image, are you sure the original image "\
+                      "is large enough to scale down this far (#{scale}) with this "\
+                      "tilesize (#{@options.tile_width}x#{@options.tile_height})?"
+            puts message
+            exit
+          end
+        end
+        # make output dir
+        make_path(task[:output_dir])
+        self.make_tiles(image, image_path, @options.tile_width, @options.tile_height)
+        image = nil
+      end
 
-      # if(Dir::exists? @options[:output_dir] == false)
-      #   Dir::mkdir(@options[:output_dir])
-      # end
+      puts "Finished."
 
-      # if @options[:auto_zoom_levels].nil? == false
-      #   # do the first zoom level at 100% of the image size
-      #   zoom_name = 20
-      #   path = File.join(@options[:output_dir], zoom_name)
-      #   Dir::mkdir(path)
-      #   self.make_tiles(@image, File.join(path, "#{@filename_prefix}"), @options[:tile_width], @options[:tile_height])
-      #   # now shrink the image by half and make that zoom level, repeat.
-      #   scale = 0.5
-      #   (@options[:auto_zoom_levels]-1).times do |level|
-      #     zoom_name = zoom_name - 1
-      #     scaled = @image.scale(scale)
-      #     path = File.join(@options[:output_dir], zoom_name)
-      #     Dir::mkdir(path)
-      #     puts "Zoom level: #{zoom_name}, scaled size: #{scaled.columns} x #{scaled.rows}"
-      #     self.make_tiles(scaled, File.join(path, "#{@filename_prefix}"), @options[:tile_width], @options[:tile_height])
-      #     scale = scale / 2
-      #   end
-      # else
-      #   Dir::mkdir(@options[:output_dir])
-      #   # no auto zoom required, just split this one image directly.
-      #   self.make_tiles(@image, File.join(@options[:output_dir], "#{@filename_prefix}"), @options[:tile_width], @options[:tile_height])
-      # end
-      
     end
 
     def make_path(directory_path)
@@ -112,7 +107,7 @@ module TileUp
       x,y,column,row = 0,0,0,0
       crops = []
 
-      puts "Tiling... columns: #{num_columns}, rows: #{num_rows}"
+      puts "Tiling image into columns: #{num_columns}, rows: #{num_rows}"
 
       while true
         x = column * tile_width
@@ -135,14 +130,12 @@ module TileUp
 
       crops.each do |c|
         ci = image.crop(c[:x], c[:y], tile_width, tile_height, true);
-        puts "Saving tile: #{c[:x]}, #{c[:y]}..."
+        print "Saving tile: #{c[:row]}, #{c[:column]}..." if @options.verbose
         ci.write("#{filename_prefix}_#{c[:column]}_#{c[:row]}.#{@extension}")
-        puts "Saved."
+        print "\rSaving tile: #{c[:row]}, #{c[:column]}... saved\n" if @options.verbose
         ci = nil
       end
     end
-
-    puts "Tiled."
   
   end
 
